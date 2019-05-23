2c9be375a319828105dd0eda370bf95a59bc672a
juliagh <50748787+juliagh@users.noreply.github.com>
2019-05-23 09:42:33 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
-function setup() {
-  createCanvas(200, 200);
+function setup() {  
+  createCanvas(400, 400);
+
 }
 
 function draw() {
-  background(220);
-}
+  background(62, 214, 221);
+  stroke(255, 200, 0);
+  strokeWeight(10);
+  fill(255, 229, 0);
+  ellipse(200, 200, 100);  
+  ellipse(200, 290, 100); 
+  ellipse(200, 390, 100); 
+  strokeWeight(15);
+  line(180, 150, 160, 100);
+  line(220, 150, 240, 100);
+  noStroke();
+  fill(0);
+  ellipse(180,190, 25);
+  ellipse(220,190, 25);
+  fill(255);
+  ellipse(183,187, 8);
+  ellipse(223,187, 8);
+  ellipse(185,193, 3);
+  ellipse(225,193, 3);
+  stroke(0);
+  strokeWeight(5);
+  line(193, 215, 207, 215);
+  line(145, 280, 120, 250);
+  line(255, 280, 280, 250);
+  }
